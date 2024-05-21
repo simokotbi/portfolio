56bf188f8eb63a47b1cf3eae3f907396b3cda452
kotbi mohamed <simojetta2@gmail.com>
2024-05-21 00:21:05 -0700

    video hero added

--- app.js
+++ app.js
@@ -13,3 +13,17 @@ function toggleMenu() {
   const menuContainer = document.querySelector('.nav-menu-items-container');
   menuContainer.classList.toggle('open');
 }
+
+window.onscroll = function() {
+  const backToTopButton = document.getElementById('backToTop');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      backToTopButton.style.display = 'block';
+  } else {
+      backToTopButton.style.display = 'none';
+  }
+};
+
+// Scroll to the top when the back-to-top button is clicked
+document.getElementById('backToTop').onclick = function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
